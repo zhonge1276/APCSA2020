89ae0e8181994e78b5ff4e0f3bf7ea62a6314615
zhonge1276 <zhonge1276@CA-SU-F106-04.sduhsd.lan>
2020-02-04 10:28:40 -0800

    git

---
+++ APCS-A-2020/src/Unit0/AsciiArt.java
@@ -0,0 +1,40 @@
+package Unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Emily Zhong \n\n" );
+		System.out.println("My Computer Monster" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("   \\   /");
+		System.out.println("#############");
+		System.out.println("##  -- --  ##");
+		System.out.println("##  {@@}   ##");
+		System.out.println("##         ##");
+		System.out.println("##   <>    ##");
+		System.out.println("##   ___   ##");
+		System.out.println("##    VV   ##");
+		System.out.println("  #########");
+		System.out.println("##   ___   ##");
+		System.out.println("##         ##");
+		System.out.println("  #########");
+		System.out.println("  #########");
+		System.out.println("##   ___   ##");
+		System.out.println("##         ##");
+		System.out.println("  #########");
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ APCS-A-2020/src/Unit0/Variables.java
@@ -0,0 +1,65 @@
+package Unit0;
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = -32123;
+		int intOne = -90877;
+		long longOne = 999999999;
+
+
+		//decimal variables
+		float floatOne = 38.5678f;
+		double doubleOne = 923.234;
+
+
+		//other integer types
+		char charOne = 'G';
+		
+
+		//other types
+		boolean booOne = true;
+		String string1 = "Hey There!";
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*          real types           *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*      other integer types      *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*          other types          *");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = "+booOne+"\t\t*");
+		System.out.println("*stringOne = "+string1+"\t\t*");
+		System.out.println("/////////////////////////////////");
+
+
+
+
+
+
+	}
+}
