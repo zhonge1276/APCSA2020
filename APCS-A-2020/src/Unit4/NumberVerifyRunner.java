c1c32c4389308dbc83a7233eb9966610fa81cb7f
zhonge1276 <zhonge1276@CA-SU-F106-04.sduhsd.lan>
2020-02-10 11:10:29 -0800

    unit4

---
+++ APCS-A-2020/src/Unit4/NumberVerifyRunner.java
@@ -0,0 +1,25 @@
+package Unit4;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date - 
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class NumberVerifyRunner
+{
+	public static void main ( String[] args )
+	{
+		Scanner keyboard = new Scanner (System.in);
+		System.out.print("Enter a whole number:: ");
+		int num = keyboard.nextInt();
+		System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println(num + " is even :: " + NumberVerify.isEven(num));
+		
+		//add in more test cases
+	}
+}
