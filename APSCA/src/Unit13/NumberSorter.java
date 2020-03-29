231be104e940310d8827df8a1349ec56e916e783
emily <emily@DESKTOP-3BNQN2O>
2020-03-29 15:17:17 -0700

    13

--- APSCA/src/Unit13/NumberSorter.java
+++ APSCA/src/Unit13/NumberSorter.java
@@ -3,18 +3,16 @@ package Unit13;
 //www.apluscompsci.com
 //Name -
 
-import java.util.Arrays; 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 import static java.lang.System.*;
 
-public class NumberSorter
-{
-	//instance variables and other methods not shown
+public class NumberSorter {
+	// instance variables and other methods not shown
 
-	private static int getNumDigits(int number)
-	{
+	private static int getNumDigits(int number) {
 		int count = 0;
 		int temp = number;
 		while (temp > 0) {
@@ -24,28 +22,26 @@ public class NumberSorter
 		return count;
 	}
 
-	public static int[] getSortedDigitArray(int number)
-	{
+	public static int[] getSortedDigitArray(int number) {
 		int length = getNumDigits(number);
 		int[] sorted = new int[length];
 		int d = 0;
-		
+
 		while (number > 0) {
 			sorted[d] = number % 10;
 			number /= 10;
 			d++;
 		}
-		
-		for (int i=1; i<sorted.length; i++) {
+
+		for (int i = 1; i < sorted.length; i++) {
 			int j = i;
-			while (j > 0 && sorted[j] < sorted[j-1]) {
+			while (j > 0 && sorted[j] < sorted[j - 1]) {
 				int temp = sorted[j];
-				sorted[j] = sorted[j-1];
-				sorted[j-1] = temp;
+				sorted[j] = sorted[j - 1];
+				sorted[j - 1] = temp;
 				j--;
 			}
 		}
-		
 		return sorted;
 	}
 }
