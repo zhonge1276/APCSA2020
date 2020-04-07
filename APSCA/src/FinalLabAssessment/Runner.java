d15f4557fcf2ab8e3d476f3f1600b375ccf5cd0c
emily <emily@DESKTOP-3BNQN2O>
2020-04-07 15:27:12 -0700

    Final Lab Assessment

--- APSCA/src/FinalLabAssessment/Runner.java
+++ APSCA/src/FinalLabAssessment/Runner.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Runner {
 	public static void main(String[] args) {
 		Scanner a;
 		try {
