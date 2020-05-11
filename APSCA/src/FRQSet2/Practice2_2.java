50bf05aa07b34f064c6c1c6399ad5661b43cbba8
emily <emily@DESKTOP-3BNQN2O>
2020-05-11 14:46:09 -0700

    frq 2 again

--- APSCA/src/FRQSet2/Practice2_2.java
+++ APSCA/src/FRQSet2/Practice2_2.java
@@ -1,5 +1,17 @@
 package FRQSet2;
 
 public class Practice2_2 {
+public static int getPeakIndex(int[] array) {
+	for (int k=1; k<array.length-1; k++) {
+		if (array[k-1] < array[k] && array[k] > array[k+1])
+			return k;
+	}
+	return -1;
+}
+
+public static boolean isMountain(int[] array) {
+	int peak = getPeakIndex(array);
+	return (peak != -1) && isIncreasing(array, peak) && isDecreasing(array, peak);
+}
 
 }
--- APSCA/src/FRQSet2/Practice2_3.java
+++ APSCA/src/FRQSet2/Practice2_3.java
@@ -1,5 +1,10 @@
 package FRQSet2;
 
 public class Practice2_3 {
-
+private double computeTemp(int row, int col) {
+	if (row == 0 || row == temps.length-1 || col == 0 || col == temps[0].length-1)
+	{
+		return temps[row][col];
+	}
+}
 }
